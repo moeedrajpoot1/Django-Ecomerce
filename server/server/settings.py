20cc1a89aac6387a68a2973ba5546ddd3cf23d80
moeed <moeedrajpoot1@gmail.com>
2024-09-30 03:50:05 +0500

    project setup

--- server/server/settings.py
+++ server/server/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'multi-vendor',                      
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
