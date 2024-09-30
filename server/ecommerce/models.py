22dc8bdff0fffd553c157d123277040c56285661
moeed <moeedrajpoot1@gmail.com>
2024-10-01 04:45:42 +0500

    product schema

--- server/ecommerce/models.py
+++ server/ecommerce/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.admin import  User
 # Create your models here.
+class Product(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    name=models.CharField(max_length=75,null=True,blank=True)
+    brand=models.CharField(max_length=75,null=True,blank=True)
+    subtitle=models.CharField(max_length=75,null=True,blank=True)
+    decsription=models.TextField(max_length=75,null=True,blank=True)
+    price=models.DecimalField(decimal_places=2,default=0,max_length=7)
+    stock=models.IntegerField(default=0)
+    rating=models.DecimalField(decimal_places=2,default=1,max_digits=5)
+    category=models.CharField(null=True,blank=True)
+    image=models.ImageField(upload_to='media')
+    
+
+    price=models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce.apps.EcommerceConfig',
+    "corsheaders",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -120,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOW_ALL_ORIGINS: True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
